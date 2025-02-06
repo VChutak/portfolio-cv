cb93e7d526f45ac90111c6c68f00c5d214c053f0
valery <valerichutak@gmail.com>
2025-02-06 13:47:13 +0200

    Redesigned seeotherporjects.

---
+++ public/data.js
@@ -0,0 +1,46 @@
+const data = {
+  bazinga: {
+    name: "Bazinga AI",
+    date: "01 2025",
+    roles: ["Web Designer", "Web Developer", "UI Designer"],
+    description: "bazinga",
+    headerDescription: `I led the end-to-end design and development of a cutting-edge digital platform, shaping its brand identity, web design, and core user experience. My role encompassed: Brand Identity Development -Crafted a cohesive visual identity, including logo, typography, and color palette, ensuring consistency across all touchpoints. Web Design & Development -Designed and developed the homepage, pricing page, and blog pages, focusing on a seamless, responsive, and engaging user experience. UI/UX Design for AI Assistant Creation Flow – Designed an intuitive and user-friendly interface for building AI assistants, optimizing the workflow for efficiency and ease of use. Through a combination of thoughtful branding, sleek UI/UX, and performant web development, I helped deliver a polished and user-centric digital experience.`,
+  },
+  writetic: {
+    name: "Writetic",
+    date: "08 2024",
+    roles: ["Web Designer", "Marketing Designer"],
+    description: "",
+    headerDescription: `Writetic is an AI-powered marketing platform designed to streamline content creation, campaign management, and team collaboration for small to medium-sized marketing teams. The platform integrates intelligent writing assistance, automated content optimization, and seamless team coordination tools to enhance productivity and creativity.`,
+  },
+  nemesysco: {
+    name: "Nemesysco",
+    date: "05 2023",
+    roles: ["UI/UX Designer"],
+    description: "",
+    headerDescription: `Nemesysco's app is a unique blend of technology and implementation. The app design needed to be simple and present a ton of information in real time, all while focusing the viewer on the important parts of the app.The challenge was to come up with a progressive disclosure of information and emphasizing different parts of the app at varying intervals.`,
+  },
+  openlm: {
+    name: "OpenLM",
+    date: "10 2021",
+    roles: ["Web Designer"],
+    description: "",
+    headerDescription: `My goal for the project was to effectively communicate the advantages of the cloud version to our existing OpenLM customers. Instead of delving into introductory details about the OpenLM product, I focused on showcasing its features directly. The design aesthetic embraced simplicity and minimalism, using a snow-white theme to instill trust and provide an authentic representation of our product's capabilities. Pricing options were transparently presented without convoluted language. In the concluding section, I included a before-and-after comparison, emphasizing the streamlined experience of the Cloud solution.`,
+  },
+  semrush: {
+    name: "Semrush",
+    date: "05 2023",
+    roles: ["Marketing Designer", "Web Designer"],
+    description: "",
+    headerDescription: `Nemesysco's app is a unique blend of technology and implementation. The app design needed to be simple and present a ton of information in real time, all while focusing the viewer on the important parts of the app.The challenge was to come up with a progressive disclosure of information and emphasizing different parts of the app at varying intervals.`,
+  },
+  automaton: {
+    name: "Automaton",
+    date: "07 2024",
+    roles: ["Web Designer", "Web Developer"],
+    description: "",
+    headerDescription: `Nemesysco's app is a unique blend of technology and implementation. The app design needed to be simple and present a ton of information in real time, all while focusing the viewer on the important parts of the app.The challenge was to come up with a progressive disclosure of information and emphasizing different parts of the app at varying intervals.`,
+  },
+};
+
+export default data;
