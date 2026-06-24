5970d070c65631887f85e0e870390050aae4078d
Valeri <valerichutak@gmail.com>
2026-06-24 11:24:32 +0200

    Redesigned everything.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+import lottie from "astro-integration-lottie";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  integrations: [
+    lottie(), // <-- add integration
+  ],
 });
